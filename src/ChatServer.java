dae33355a7814576355def426d715fc40430e32f
tlyong1992 <tlyong1992@hotmail.com>
2016-06-09 23:27:19 +0800

    V0.8 服务端Server引入多线程处理客户端请求

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -20,14 +20,16 @@ public class ChatServer {
 
 	static Integer port = 123;
 	static ServerSocket ss =null;
-	static DataInputStream dis = null;
 	static Socket s = null;
+	static ServerWindow serverWindow = new ServerWindow();
 	
 	public static void main(String[] args) {
+		new ChatServer().start();
+	}
+	
+	public void start(){
 		
-		ChatServer chatServer = new ChatServer();
-		
-		ServerWindow serverWindow = new ServerWindow();
+		//生成服务器的可视化界面
 		serverWindow.init();
 		
 		try {
@@ -40,56 +42,88 @@ public class ChatServer {
 			serverWindow.textArea.setText(serverWindow.textArea.getText()+e2.toString()+"\n");
 		}
 
-		//用死循环接收
+		//用死循环等待用户的连接
 		while(!ss.isClosed()){
 			//接受到一个socket
 			try{
-				s = ss.accept();
-				serverWindow.textArea.setText(serverWindow.textArea.getText()+"\n"+s+"客户端连接\n");
-				dis = new DataInputStream(s.getInputStream());
+				System.out.println("进入等待状态");
+				s = ss.accept();//阻塞方法
+				if(s.isConnected()){
+					Service service = new Service(this);
+					new Thread(service).start();
+				}
 			} catch (IOException e4) {
 				serverWindow.textArea.setText(serverWindow.textArea.getText()+e4.toString()+"连接中断！"+"\n");
-				chatServer.disconnect();
 			}
 			
+		}
+	}
+	
+	public class Service implements Runnable{
+		
+		public Socket s = null;
+		public ServerWindow serverWindow = null;
+		public DataInputStream dis = null;
+		
+		public Service(ChatServer chatServer) {
+			//传入服务器接受到的客户端socke 以及界面；
+			this.s = chatServer.s;
+			this.serverWindow = chatServer.serverWindow;
+		}
+		
+		//运行服务
+		@Override
+		public void run() {
+			
+			serverWindow.textArea.setText(serverWindow.textArea.getText()+"\n"+s+"客户端连接\n");
 			//对收到的数据进行处理
 			while (s!=null) {
+				try {
+					dis = new DataInputStream(s.getInputStream());
+				} catch (IOException e) {
+					serverWindow.textArea.setText(serverWindow.textArea.getText()+e.toString()+"\n连接中断\n");
+					this.disconnect();
+				}
 				if (!s.isClosed()) {
 					try{
+						//阻塞函数：运行卡在这里
 						String message = dis.readUTF();
 						serverWindow.textArea.setText(serverWindow.textArea.getText()+"客户端："+message+"\n");
 					}catch(EOFException e1){
-						chatServer.disconnect();
+						this.disconnect();
 						System.out.println("客户端断开！");
 						serverWindow.textArea.setText(serverWindow.textArea.getText()+"客户端已断开！"+"\n");
 					} catch (IOException e) {
 						serverWindow.textArea.setText(serverWindow.textArea.getText()+e.toString()+"错误！"+"\n");
-						chatServer.disconnect();
+						this.disconnect();
 					} 
 				}
 			}
 		}
-	}
-	
-	public void disconnect(){
-		try{
-			if(s!=null){
-				s.close();
-			}
-			if(dis!=null){
-				dis.close();
-			}
-		}catch(IOException e){
-			System.out.println("关闭连接错误");
-		}finally{
-			if(s!=null){
-				s=null;
-			}
-			if(dis!=null){
-				dis=null;
+		
+		//断开连接
+		public void disconnect(){
+			try{
+				if(s!=null){
+					s.close();
+				}
+				if(dis!=null){
+					dis.close();
+				}
+			}catch(IOException e){
+				System.out.println("关闭连接错误");
+			}finally{
+				if(s!=null){
+					s=null;
+				}
+				if(dis!=null){
+					dis=null;
+				}
 			}
 		}
+		
 	}
+	
 	//窗体内部类
 	private static class ServerWindow extends JFrame{
 		
