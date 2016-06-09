2cfd9a0bc6381684cf891e162d524be2b8602bc9
tlyong1992 <tlyong1992@hotmail.com>
2016-06-09 16:18:34 +0800

    V0.2-添加输入框按键监听

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,20 +1,23 @@
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
+
 import java.awt.BorderLayout;
+
 import javax.swing.JTextField;
+import javax.tools.JavaCompiler;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.beans.PropertyChangeListener;
 
 public class ChatClient extends JFrame{
-
-	private static final long serialVersionUID = 1L;
 
 	JTextField textField = new JTextField();
 	JTextArea textArea = new JTextArea();
 	
-	public static void main(String[] args) {
-		new ChatClient().launchFrame();
-	}
-	
-	public void launchFrame(){
+	public ChatClient() {
 		this.setLocation(400, 200);
 		this.setSize(400, 300);
 		this.setTitle("客户端1");
@@ -22,10 +25,72 @@ public class ChatClient extends JFrame{
 		getContentPane().add(textArea,BorderLayout.CENTER);
 		getContentPane().add(textField,BorderLayout.SOUTH);
 //		this.pack();
+		
+		textField.addActionListener(new TFListener());
+		
 		this.setVisible(true);
 		
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
 		
+	}
+
+	private class TFListener implements javax.swing.Action{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String string = textField.getText().trim();
+			textArea.setText(string);
+			textField.setText("");
+		}
+
+		@Override
+		public Object getValue(String key) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void putValue(String key, Object value) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setEnabled(boolean b) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public boolean isEnabled() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public void addPropertyChangeListener(PropertyChangeListener listener) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void removePropertyChangeListener(PropertyChangeListener listener) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
 		
+	}
+	
+	private static final long serialVersionUID = 1L;
+	public static void main(String[] args) {
+		new ChatClient();
 	}
 }
   
