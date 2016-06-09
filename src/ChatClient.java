bf6718fbdf96eb24282eb65ee2e2e568a30e015e
tlyong1992 <tlyong1992@hotmail.com>
2016-06-09 20:18:02 +0800

    V0.6 【更新】客户端显示当前连接状态； 【修复BUG】服务端检测到客户端断开后立即关闭s和dis；客户端可以重复断开和连接

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -9,10 +9,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.beans.PropertyChangeListener;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ConnectException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
@@ -21,20 +23,98 @@ import javax.swing.JMenuItem;
 import javax.swing.ButtonGroup;
 import javax.swing.JMenu;
 import javax.swing.JScrollPane;
+import javax.swing.JComboBox;
+import javax.swing.JPopupMenu;
+
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JLabel;
 
 public class ChatClient extends JFrame{
 
 	JTextField textField = new JTextField();
 	JTextArea textArea = new JTextArea();
-	JMenu menu = new JMenu("网络");
-	JButton button = new JButton("连接服务端");
+	Socket s = null;
+	DataOutputStream dos = null;
+	String serverURL = "127.0.0.1";
+	int port = 123;
 	
-	//在构造函数里初始化
+	//连接服务端
+	private void connect(){
+		textArea.setText(textArea.getText()+"正在连接服务端...."+"\n");
+		try {
+			//连接的同时保存下输出流的引用
+			s = new Socket(serverURL, port);
+			if(s.isConnected()){
+				textArea.setText(textArea.getText()+"服务器连接成功...."+"\n");
+				setTitle("客户端:已连接");
+			}
+			dos = new DataOutputStream(s.getOutputStream());
+		}catch(ConnectException e1){
+			textArea.setText(textArea.getText()+"在"+serverURL+":"+ port + " 上没有找到服务器...."+"\n");
+		}catch (SocketException e) {
+			textArea.setText(textArea.getText()+"已从服务器断开连接"+"\n");
+		}
+		catch (IOException e2) {
+			// TODO Auto-generated catch block
+			textArea.setText(textArea.getText()+"连接错误...."+"\n"+e2.toString());
+		}
+	}
+	
+	//断开连接
+	private void disConnect(){
+		if(s!=null && dos !=null){
+			try {
+				dos.close();
+				s.close();
+				textArea.setText(textArea.getText()+"已断开与服务器的连接\n");
+				setTitle("客户端:连接已断开");
+			} catch (IOException e) {
+				textArea.setText(textArea.getText()+e.toString()+"并没有连接\n");
+			}
+		}else {
+			textArea.setText(textArea.getText()+"并没有连接\n");
+		}
+		
+	}
+	
+	//输入框响应发送
+	private void send(){
+		String string = textField.getText().trim();
+		textArea.setText(textArea.getText()+string+"\n");
+		textField.setText("");
+		//一个输出流
+		try {
+			if(s!=null && !s.isClosed()){
+			dos.writeUTF(string);
+			dos.flush();
+//			dos.close();
+			}else{
+				textArea.setText(textArea.getText()+"未连接服务器"+"\n");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static final long serialVersionUID = 1L;
+	private final JMenu netMenu = new JMenu("网络");
+	private final JMenuItem connectMenuItem = new JMenuItem("连接");
+	private final JMenuItem disconnectMenuItems = new JMenuItem("断开");
+	
+	public static void main(String[] args) {
+		ChatClient chatClient = new ChatClient();
+	}
+	
+	
+	//在构造函数里初始化窗口
 	public ChatClient() {
 		//设置尺寸标题
-		this.setLocation(400, 200);
-		this.setSize(400, 300);
-		this.setTitle("客户端1");
+		setLocation(480, 80);
+		setSize(400, 300);
+		setTitle("客户端:未连接服务器");
 		
 		//布局管理器
 		textArea.setEditable(false);
@@ -46,56 +126,43 @@ public class ChatClient extends JFrame{
 		textField.addActionListener(new ActionListener() {
 			//textField 预先响应了回车键事件
 			public void actionPerformed(ActionEvent e) {
-				String string = textField.getText().trim();
-				textArea.setText(textArea.getText()+string+"\n");
-				textField.setText("");
+				send();
 			}
+
 		});
-		
 		getContentPane().add(textField,BorderLayout.SOUTH);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
-		menuBar.add(menu);
-		button.addActionListener(new ActionListener() {
+		menuBar.add(netMenu);
+		connectMenuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				connect();
 			}
 		});
 		
-		menu.add(button);
+		netMenu.add(connectMenuItem);
+		disconnectMenuItems.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				disConnect();
+			}
+		});
 		
-//		this.pack();
+		netMenu.add(disconnectMenuItems);
 		
-		this.setVisible(true);
+		setVisible(true);
 		
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
 				System.out.println("客户端退出..");
+				disConnect();
 				System.exit(0);
 			}
 		});
-		
 	}
 	
-	//连接函数
-	private void connect(){
-		try {
-			textArea.setText(textArea.getText()+"\n"+"正在连接服务端....");
-			Socket s = new Socket("127.0.0.1", 123);
-			if(s.isConnected()){
-				textArea.setText(textArea.getText()+"\n"+"服务器连接成功....");
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	private static final long serialVersionUID = 1L;
-	public static void main(String[] args) {
-		ChatClient chatClient = new ChatClient();
-	}
+	
 }
   
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,61 +1,94 @@
+import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 import java.awt.BorderLayout;
-import java.awt.Rectangle;
+import java.awt.Color;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import javax.swing.JScrollBar;
-
-
 public class ChatServer {
-	
-	/**
-	 * @wbp.parser.entryPoint
-	 */
+
 	public static void main(String[] args) {
 		
 		ServerWindow serverWindow = new ServerWindow();
 		serverWindow.init();
-		serverWindow.textArea.setText("服务器已经启动！------");
+		Integer port = 123;
+		ServerSocket ss =null;
+		DataInputStream dis = null;
+		Socket s = null;
 		
 		try {
 			//创建server
-			ServerSocket ss = new ServerSocket(123);
-			//用死循环接受
-			while(true){
-				//接受到一个socket
-				Socket socket = ss.accept();
-				serverWindow.textArea.setText(serverWindow.textArea.getText()+"\n一个客户端已经连接====================");
-				serverWindow.textArea.setText(serverWindow.textArea.getText()+"\n"+socket);
+			ss = new ServerSocket(port);
+			serverWindow.textArea.setText("服务器启动成功！"+ss.getLocalSocketAddress().toString()+"\n");
+		}catch(SocketException e1){
+			serverWindow.textArea.setText(serverWindow.textArea.getText()+"无法启动服务器，端口号: "+port+" 已被占用"+"\n");
+		}catch (IOException e2) {
+			serverWindow.textArea.setText(serverWindow.textArea.getText()+e2.toString()+"\n");
+		}
+		
+
+		//用死循环接收
+		while(!ss.isClosed()){
+			//接受到一个socket
+			try{
+				s = ss.accept();
+				serverWindow.textArea.setText(serverWindow.textArea.getText()+"\n"+s+"客户端连接\n");
+				dis = new DataInputStream(s.getInputStream());
+			} catch (IOException e4) {
+				try{
+					s.close();
+					dis.close();
+				}catch(IOException e5){
+					serverWindow.textArea.setText(serverWindow.textArea.getText()+e5.toString()+"关闭连接错误！"+"\n");
+				}
+				serverWindow.textArea.setText(serverWindow.textArea.getText()+e4.toString()+"连接中断！"+"\n");
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
+			//对收到的数据进行处理
+			while(!s.isClosed()){
+				try{
+					String inString = dis.readUTF();
+					serverWindow.textArea.setText(serverWindow.textArea.getText()+"客户端："+inString+"\n");
+				}catch(EOFException e1){
+					try{
+						s.close();
+						dis.close();
+					}catch(IOException e5){
+						serverWindow.textArea.setText(serverWindow.textArea.getText()+e5.toString()+"关闭连接错误！"+"\n");
+					}
+					serverWindow.textArea.setText(serverWindow.textArea.getText()+e1.toString()+"客户端已断开！"+"\n");
+				} catch (IOException e) {
+					serverWindow.textArea.setText(serverWindow.textArea.getText()+e.toString()+"错误！"+"\n");
+				}
+			}
+		
 		}
 	}
-	
+	//窗体内部类
 	private static class ServerWindow extends JFrame{
 		
 		private static final long serialVersionUID = 1L;
-		
 		JTextArea textArea = new JTextArea();
 		
 		public void init() {
 			
 			setTitle("聊天室服务端");
-			setLocation(400, 200);
+			setLocation(80, 80);
 			setSize(400, 300);
 			setVisible(true);
 			
 			//布局
 			textArea.setLineWrap(true);//激活自动换行
-			textArea.setWrapStyleWord(true);//激活断行不断字
+			textArea.setBackground(new Color(64, 224, 208));
+//			textArea.setWrapStyleWord(true);//激活断行不断字
 			JScrollPane scrollPane = new JScrollPane(textArea);
 			add(scrollPane, BorderLayout.CENTER);
 			addWindowListener(new WindowAdapter() {
@@ -69,5 +102,6 @@ public class ChatServer {
 		}
 		
 	}
+
 }
   
